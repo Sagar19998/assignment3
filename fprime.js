c269153487911419543ed59877eb5571b7fcf589
jayeshdeshmukh18 <deshmukhjayesh507@gmail.com>
2022-09-20 22:38:48 +0530

    prime check

--- fprime.js
+++ fprime.js
@@ -1,8 +1,20 @@
-function prime(num)
+function prime(num){
+  let counter=0
   for (let i=1;i<=num;i++){
-    if (num%1){
-      return true;
+    if (num%1==0){
+      counter++;
     }
   }
-return false;
+  if (counter==2){
+    return true
+  }
+  return false
+}
+
+let ans=prime(7);
+if(ans==true){
+  console.log("prime");
+}
+else{
+  console.log("Not prime")
 }
