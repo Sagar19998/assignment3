0b4710cba4da22c22ede3664897f01dab1dd4af5
sagar jadhao <jadhaosagar9798@gmail.com>
2022-09-20 23:19:16 +0530

    code fixed by sagar

--- fprime.js
+++ fprime.js
@@ -1,8 +1,21 @@
-function prime(num)
-  for (let i=1;i<=num;i++){
-    if (num%1){
-      return true;
+function prime(number)
+factors=0;
+  for (let i=1;i<=number;i++){
+    if (number%1==0){
+      factors++;
+      
     }
   }
+  if (factors==2){
+    return true;
+  }
 return false;
+}
+let ans=prime(7);
+if(ans==true){
+  console.log("prime");
+
+}
+else{
+  console.log("Not prime");
 }
